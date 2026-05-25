3d02145b0c857cf9982690df076a17290dd77bfe
dukungin <dukung.in.support@gmail.com>
2026-05-25 11:25:48 +0700

    fix tts

--- src/pages/storeManager.jsx
+++ src/pages/storeManager.jsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Plus, Trash2, Save, ExternalLink } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import api from '../lib/axiosInstance';
 
 const StoreManager = ({ overlayToken }) => {
