a7c6c36ea416e4b8ea5fd49da4141905b2e5024f
dukungin <dukung.in.support@gmail.com>
2026-06-02 09:58:32 +0700

    fix ui homapge

--- src/pages/taptiptup.jsx
+++ src/pages/taptiptup.jsx
@@ -1126,7 +1126,7 @@ export default function TapTipTup() {
         <div style={{
           position: "fixed",
           top: 0, left: 0, right: 0, bottom: 0,
-          background: "rgba(0,0,0,0.85)",
+          background: "rgba(0,0,0,0.92)",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
